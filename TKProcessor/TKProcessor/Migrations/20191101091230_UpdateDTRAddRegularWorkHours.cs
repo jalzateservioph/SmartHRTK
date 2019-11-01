ee4f54099377fac8d1c9ecdb17126605a75cb993
Josh <JoshJ.alzate77@gmail.com>
2019-11-01 17:12:54 +0800

    Added regular work hours field migration

---
+++ TKProcessor/TKProcessor/Migrations/20191101091230_UpdateDTRAddRegularWorkHours.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TKProcessor.Migrations
+{
+    public partial class UpdateDTRAddRegularWorkHours : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "RegularWorkHours",
+                table: "DailyTransactionRecord",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegularWorkHours",
+                table: "DailyTransactionRecord");
+        }
+    }
+}
