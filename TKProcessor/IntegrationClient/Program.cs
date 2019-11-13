d8000fe380741789668d9c1bfc97d3f08c2fa3a4
Josh <JoshJ.alzate77@gmail.com>
2019-11-13 10:14:34 +0800

    Created projects for biometrics integration

---
+++ TKProcessor/IntegrationClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntegrationClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
