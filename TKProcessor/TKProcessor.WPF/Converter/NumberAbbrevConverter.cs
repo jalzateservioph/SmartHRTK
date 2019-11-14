c1d328edcc048e33a61091d450846f1dad398eae
Josh <JoshJ.alzate77@gmail.com>
2019-11-14 19:52:35 +0800

    Started dashboard functionality creation Cosmetic updates

---
+++ TKProcessor/TKProcessor.WPF/Converter/NumberAbbrevConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TKProcessor.WPF.Converter
+{
+    public class NumberAbbrevConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal)
+                return FormatDecimal((decimal)value);
+
+            return FormatInt((int)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        static string FormatInt(int num)
+        {
+            if (num >= 100000)
+                return FormatInt(num / 1000) + "K";
+            if (num >= 10000)
+            {
+                return (num / 1000D).ToString("0.#") + "K";
+            }
+            return num.ToString("#,0");
+        }
+
+        static string FormatDecimal(decimal num)
+        {
+            if (num >= 100000)
+                return FormatDecimal(num / 1000) + "K";
+            if (num >= 10000)
+            {
+                return (num / 1000M).ToString("0.#") + "K";
+            }
+            return num.ToString("#,0");
+        }
+    }
+}
--- TKProcessor/TKProcessor.WPF/ViewModels/DashboardViewModel.cs
+++ TKProcessor/TKProcessor.WPF/ViewModels/DashboardViewModel.cs
@@ -4,12 +4,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TKProcessor.Services;
+using TKProcessor.Services.Maintenance;
+using TKProcessor.WPF.Common;
 using TKProcessor.WPF.Models;
 
 namespace TKProcessor.WPF.ViewModels
 {
     public class DashboardViewModel : Conductor<Dashboard>
     {
+        readonly EmployeeService employeeService;
+        readonly DailyTransactionRecordService dtrService;
+        private DateTime startDate;
+        private DateTime endDate;
 
+        public DashboardViewModel()
+        {
+            employeeService = new EmployeeService(Session.Default.CurrentUser?.Id ?? Guid.Empty);
+
+            dtrService = new DailyTransactionRecordService(Session.Default.CurrentUser?.Id ?? Guid.Empty);
+
+            StartDate = DateTime.Today.AddDays(-DateTime.Today.Day);
+
+            EndDate = DateTime.Today.AddDays(-DateTime.Today.Day).AddMonths(1).AddDays(-1);
+
+            PropertyChanged += DashboardViewModel_PropertyChanged;
+        }
+
+        private void DashboardViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if((e.PropertyName == nameof(StartDate) || e.PropertyName == nameof(EndDate)) &&
+                StartDate > EndDate)
+            {
+                EndDate = StartDate;
+            }
+        }
+
+        public void Populate()
+        {
+            NotifyOfPropertyChange(() => EmployeeCount);
+        }
+
+        public DateTime StartDate
+        {
+            get => startDate;
+            set
+            {
+                startDate = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public DateTime EndDate
+        {
+            get => endDate;
+            set
+            {
+                endDate = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public int EmployeeCount { get => employeeService.List().Count(); }
+
+        public int ActiveEmployeeCount { get => employeeService.List().Where(i => i.TerminationDate == null).Count(); }
+
+        public decimal HoursWorked { get => dtrService.List().Sum(i => i.RegularWorkHours); }
+
+        public int AbsentEmployeeCount { get; }
+
+        public decimal AbsentHoursCount { get; }
     }
 }
