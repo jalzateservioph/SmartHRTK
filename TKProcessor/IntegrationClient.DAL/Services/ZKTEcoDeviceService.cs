51e3ce8de7c2ab9a179e10f9315244ed345411cd
gandezajasper@gmail.com <gandezajasper@gmail.com>
2019-11-18 13:36:05 +0800

    IntegrationClient ZKTEco device interaction logic, command line args parsing, user interaction logic

--- TKProcessor/IntegrationClient.DAL/Services/ZKTEcoDeviceService.cs
+++ TKProcessor/IntegrationClient.DAL/Services/ZKTEcoDeviceService.cs
@@ -3,7 +3,9 @@ using IntegrationClient.DAL.Models;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using zkemkeeper;
 
 namespace IntegrationClient.DAL.Services
 {
@@ -11,21 +13,162 @@ namespace IntegrationClient.DAL.Services
     {
         private IConfiguration _configuration;
         private ILoggingService _loggingService;
+        private CZKEMClass device;
+        private readonly int machineNumber = 1; //Not sure why this is 1 but this is what is set on the SDK.
 
         public ZKTEcoDeviceService(IConfiguration configuration, ILoggingService loggingService)
         {
             _configuration = configuration;
             _loggingService = loggingService;
+            device = new CZKEMClass();
         }
 
         public IEnumerable<RawData> GetRawData(DateTime? from, DateTime? to)
         {
-            return null;        
+            ConnectToDevice();
+            //Disable the device
+            EnableDevice(false);
+            //Get records
+            List<RawData> rawData = new List<RawData>();
+            _loggingService.Log("Reading raw data from device...", Enums.LogLevel.Info);
+            string sdwEnrollNumber = "";
+            int idwVerifyMode = 0;
+            int idwInOutMode = 0;
+            int idwYear = 0;
+            int idwMonth = 0;
+            int idwDay = 0;
+            int idwHour = 0;
+            int idwMinute = 0;
+            int idwSecond = 0;
+            int idwWorkcode = 0;
+
+            if (from.HasValue && to.HasValue) //Filter by date
+            {
+                if (device.ReadTimeGLogData(machineNumber, from.Value.ToString("yyyy-MM-dd HH:mm:ss"), to.Value.ToString("yyyy-MM-dd HH:mm:ss")))
+                {
+                    while (device.SSR_GetGeneralLogData(machineNumber, out sdwEnrollNumber, out idwVerifyMode, out idwInOutMode, out idwYear, out idwMonth, out idwDay, out idwHour, out idwMinute, out idwSecond, ref idwWorkcode))
+                    {
+                        RawData raw = new RawData()
+                        {
+                            EmployeeBiometricsID = sdwEnrollNumber,
+                            TransactionDateTime = new DateTime(idwYear, idwMonth, idwDay, idwHour, idwMinute, idwSecond)
+                        };
+
+                        rawData.Add(raw);
+                    }
+                }
+            }
+            else
+            {
+                if (device.ReadGeneralLogData(machineNumber))
+                {
+
+
+                    while (device.SSR_GetGeneralLogData(machineNumber, out sdwEnrollNumber, out idwVerifyMode, out idwInOutMode, out idwYear, out idwMonth, out idwDay, out idwHour, out idwMinute, out idwSecond, ref idwWorkcode))
+                    {
+                        RawData raw = new RawData()
+                        {
+                            EmployeeBiometricsID = sdwEnrollNumber,
+                            TransactionDateTime = new DateTime(idwYear, idwMonth, idwDay, idwHour, idwMinute, idwSecond)
+                        };
+
+                        rawData.Add(raw);
+                    }
+                }
+            }
+            
+            
+            //Enable the device
+            EnableDevice(true);
+            DisconnectFromDevice();
+            return rawData;        
         }
 
         public void EnrollUsers(IEnumerable<Employee> employees) 
         {
-            
+            if (employees != null)
+            {
+                _loggingService.Log($"Employees count: {employees.Count()}", Enums.LogLevel.Info);
+                foreach (var employee in employees)
+                {
+                    ConnectToDevice();
+                    EnableDevice(false);
+                    device.SetStrCardNumber("");
+                    if (device.SSR_SetUserInfo(machineNumber, employee.BiometricsId, employee.EmployeeName, "123", 0, true))
+                    {
+                        _loggingService.Log($"Successfully created/updated user {employee.BiometricsId} - {employee.EmployeeName}", Enums.LogLevel.Info);
+                    }
+                    else
+                    {
+                        _loggingService.Log($"Failed creating/updating user {employee.BiometricsId} - {employee.EmployeeName}", Enums.LogLevel.Warn);
+                    }
+                    EnableDevice(true);
+                }
+
+                DisconnectFromDevice();
+            }
+
         }
+
+        private void ConnectToDevice()
+        {
+            string config = _configuration.GetConnectionString("ZKTEcoDevice");
+
+            string[] _config = config.Split(';');
+            bool invalid = false;
+            string IP = "";
+            int port = 0;
+            int commKey = 0;
+
+            if (_config.Length != 3)
+            {
+                invalid = true;
+            }
+            else
+            {
+                IP = _config[0];
+
+                if (!int.TryParse(_config[1], out port))
+                {
+                    invalid = true;
+                }
+                if (!int.TryParse(_config[2], out commKey))
+                {
+                    invalid = true;
+                }
+            }
+
+
+            if (invalid)
+            {
+                _loggingService.Log("Invalid device connection string", Enums.LogLevel.Fatal);
+                throw new Exception("Invalid device connection string");
+            }
+            device.SetCommPassword(commKey);
+
+
+            _loggingService.Log("Connecting to Device...", Enums.LogLevel.Info);
+            if (device.Connect_Net(IP, port))
+            { 
+                _loggingService.Log("Connection successful", Enums.LogLevel.Info);
+            }
+            else
+            {
+                _loggingService.Log("Connection failed", Enums.LogLevel.Fatal);
+                throw new Exception("Connection to device failed");
+            }
+        }
+
+        private void DisconnectFromDevice()
+        {
+            device.Disconnect();
+        }
+
+
+        private void EnableDevice(bool enable)
+        {
+            device.EnableDevice(machineNumber, enable);
+        }
+
     }
 }
--- TKProcessor/IntegrationClient/Program.cs
+++ TKProcessor/IntegrationClient/Program.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.Configuration;
 using System.IO;
 using IntegrationClient.DAL.Interfaces;
 using IntegrationClient.DAL.Services;
+using System.Collections.Generic;
+using IntegrationClient.DAL.Models;
+using System.Linq;
 namespace IntegrationClient
 {
     class Program
@@ -17,9 +20,7 @@ namespace IntegrationClient
             RegisterServices();
 
             ILoggingService loggingService = _serviceProvider.GetService<ILoggingService>();
-            //Test connection to both TK and Device
-            
-
+            IDeviceService deviceService = _serviceProvider.GetRequiredService<IDeviceService>();
 
             if (args.Length == 0) //Invoked by user
             {
@@ -33,10 +34,64 @@ namespace IntegrationClient
                 {
                     if (parsedInput == 1) //Pull employee data
                     {
+                        //Connect to WebAPI
+                        deviceService.EnrollUsers(null);
 
                     }
                     else if (parsedInput == 2) //Push raw data
                     {
+                        Start2:
+                        Console.WriteLine("Please input date range. First date value is from and second is to. Separate values using comma(,). If no range is supplied all records will be pushed to TK, \nFormat: yyyy-MM-dd");
+                        input = Console.ReadLine();
+
+                        string[] _input = input.Split(',');
+                        if (_input.Length == 2)
+                        {
+                            DateTime from;
+                            DateTime to;
+
+                            if (!DateTime.TryParse(_input[0], out from) || !DateTime.TryParse(_input[1], out to))
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Invalid input");
+                                goto Start2;
+                            }
+
+                            if (to < from)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Date range end should not be less than its start");
+                                goto Start2;
+                            }
+                            try
+                            {
+                                IEnumerable<RawData> rawData = deviceService.GetRawData(from, to);
+
+                                //push data to WebAPI
+                            }
+                            catch (Exception ex)
+                            {
+
+                            }
+                        }
+                        else if (String.IsNullOrWhiteSpace(input))
+                        {
+                            try
+                            {
+                                IEnumerable<RawData> rawData = deviceService.GetRawData(null, null);
+                                //push data to WebAPI
+                            }
+                            catch (Exception ex)
+                            {
+
+                            }
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Invalid input");
+                            goto Start2;
+                        }
 
                     }
                     else
@@ -72,14 +127,80 @@ namespace IntegrationClient
                  *      
                  *      
                  */
-            }
+                if (args[0].ToLower().Equals("pull-employeedata"))
+                {
+                    //Connect to WebAPI
+                    deviceService.EnrollUsers(null);
+                }
+                else if (args[0].ToLower().Equals("push-rawdata"))
+                {
+                    //arg format for date range should be from=yyyy-MM-dd to=yyyy-MM-dd
+                    if (args.Length == 1) //No date range
+                    {
+                        IEnumerable<RawData> rawData = deviceService.GetRawData(null, null);
+                        //push data to WebAPI
+                    }
+                    else
+                    {
+                        string dateFrom = args.Where(s => s.ToLower().StartsWith("from")).FirstOrDefault();
+                        string dateTo = args.Where(s => s.ToLower().StartsWith("to")).FirstOrDefault();
 
+                        if (String.IsNullOrWhiteSpace(dateFrom) || String.IsNullOrWhiteSpace(dateTo))
+                        {
+                            loggingService.Log("Invalid arguments", DAL.Enums.LogLevel.Fatal);
+                        }
+                        else
+                        {
+                            string[] _dateFrom = dateFrom.Split('=');
+                            string[] _dateTo = dateTo.Split('=');
+
+                            if (_dateFrom.Length == 2 && _dateTo.Length == 2)
+                            {
+                                DateTime from;
+                                DateTime to;
+
+                                if (!DateTime.TryParse(_dateFrom[1], out from) || !DateTime.TryParse(_dateTo[1], out to))
+                                {
+                                    loggingService.Log("Invalid arguments", DAL.Enums.LogLevel.Fatal);
+                                    return;
+                                }
+
+                                if (to < from)
+                                {
+                                    loggingService.Log("Invalid arguments", DAL.Enums.LogLevel.Fatal);
+                                    return;
+                                }
+                                try
+                                {
+                                    IEnumerable<RawData> rawData = deviceService.GetRawData(from, to);
+
+                                    //push data to WebAPI
+                                }
+                                catch (Exception ex)
+                                {
+
+                                }
+                            }
+                            else
+                            { 
+                                loggingService.Log("Invalid arguments", DAL.Enums.LogLevel.Fatal);
+                            }
+                        }
+
+
+                    }
+                }
+                else 
+                {
+                    loggingService.Log("Invalid arguments", DAL.Enums.LogLevel.Fatal);
+                }
+            }
             DisposeServices();
          }
 
         private static void BuildConfiguration()
         {
-            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
             _configuration = builder.Build();
         }
 
@@ -87,9 +208,10 @@ namespace IntegrationClient
         {
             var collection = new ServiceCollection();
 
-            collection.AddScoped<IConfiguration>(_ => _configuration);
+            collection.AddSingleton<IConfiguration>(_ => _configuration);
             collection.AddSingleton<ILoggingService,NLogService>();
             collection.AddSingleton<IEmployeeService, WebAPIEmployeeService>();
+            collection.AddSingleton<IDeviceService, ZKTEcoDeviceService>(); //Change this depending on the device
 
             //add services here
 
