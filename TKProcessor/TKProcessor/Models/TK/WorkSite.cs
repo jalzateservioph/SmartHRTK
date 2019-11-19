f9e796ea4f56e24cf938be11c329b7b9a8a7dcf0
Kanon <akr.shrgn@gmail.com>
2019-11-20 07:39:15 +0800

    Added worksite relationship to employee entity

---
+++ TKProcessor/TKProcessor/Models/TK/WorkSite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TKProcessor.Models.TK
+{
+    public class WorkSite : IEntity
+    {
+        public WorkSite()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public User CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public User LastModifiedBy { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+        public bool IsActive { get; set; }
+
+        public string SiteId { get; set; }
+        public string Name { get; set; }
+    }
+}
