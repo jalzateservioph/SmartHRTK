b2ef85d29958afcb4371413ca3c8c06f265d311c
Josh <JoshJ.alzate77@gmail.com>
2019-11-19 13:51:23 +0800

    Fixed minutes display issue in DTR

---
+++ TKProcessor/TKProcessor.Common/TimeClasses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TKProcessor.Common
+{
+    public static class TimeSpanExtensions
+    {
+        public static TimeSpan RoundToNearestMinutes(this TimeSpan input, int minutes)
+        {
+            var totalMinutes = (int)(input + new TimeSpan(0, minutes / 2, 0)).TotalMinutes;
+
+            return new TimeSpan(0, totalMinutes - totalMinutes % minutes, 0);
+        }
+    }
+}
