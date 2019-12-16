ae7b4b895c80c1a182fe9bea442799e193a4db24
Josh <JoshJ.alzate77@gmail.com>
2019-12-16 12:03:13 +0800

    Created db generator project

---
+++ TKProcessor/TKProcessor.DBGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TKProcessor.Services.Maintenance;
+
+namespace TKProcessor.DBGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Timekeepin Database Generator");
+
+            new Program();
+        }
+
+        public Program()
+        {
+            Console.WriteLine("Generating/Updating database...");
+
+            DatabaseService.Migrate();
+
+            Console.WriteLine("Database created. Press any key to close.");
+
+            Console.ReadLine();
+        }
+    }
+}
